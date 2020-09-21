7cd29a998aa3c78543a793ae5d027b6ceb5502dc
Brendan Bosman <bhbosman@gmail.com>
2020-09-21 20:07:25 +0200

    Change dependencies

---
+++ ReaderWriterFactory_test.go
@@ -0,0 +1,32 @@
+package gomessageblock
+
+import (
+	"context"
+	"github.com/bhbosman/gologging"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+	"testing"
+)
+
+func TestWriterFactoryService(t *testing.T) {
+	var sut IReaderWriterFactoryService
+	app := fxtest.New(
+		t,
+		gologging.ProvideLogFactoryForTesting(t, nil),
+		ProvideReaderWriterFactoryService(),
+		fx.Populate(&sut),
+	)
+	if !assert.NoError(t, app.Err()) {
+		return
+	}
+	if !assert.NoError(t, app.Start(context.TODO())) {
+		return
+	}
+	defer func() {
+		assert.NoError(t, app.Start(context.TODO()))
+	}()
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/bhbosman/gomessageblock
 
 go 1.15
+
+require (
+	github.com/bhbosman/gologging v0.0.0-20200921180328-d29fc55c00bc
+	github.com/golang/mock v1.4.4
+	github.com/stretchr/testify v1.6.1
+	go.uber.org/fx v1.13.1
+)
